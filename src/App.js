975575d35d961407fb9d51d01018f915992f889d
RobbyCL21 <cloetensrobby@gmail.com>
2025-02-27 16:44:00 +0100

    the different pages

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Welcome to my personal love letter to my beloved Virginia
-    </div>
+    <>
+      <div className="App">
+        Welcome to my personal love letter to my beloved Virginia
+      </div>
+      <div className="DifferentPages">
+        Here come the different pages
+      </div>
+    </>
   );
 }
 
