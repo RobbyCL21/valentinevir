3499cb65a8793bef124ff613fff2dfa26b127cb8
RobbyCL21 <cloetensrobby@gmail.com>
2025-03-12 16:47:29 +0100

    removing unneeded imports

--- src/components/findapair.js
+++ src/components/findapair.js
@@ -1,7 +1,3 @@
-import logo from './logo.svg';
-import loveletter from './loveletter.png';
-import findapair from './find a pair.png';
-import { Link } from "react-router-dom";
 import './css/App.css';
 
 function FindAPair() {
--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import loveletter from './loveletter.png';
 import findapair from './find a pair.png';
 import { Link } from "react-router-dom";
