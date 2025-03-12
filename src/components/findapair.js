1839e83629e7d41608a0354138e8d5cc44e08c63
RobbyCL21 <cloetensrobby@gmail.com>
2025-03-12 20:40:20 +0100

    fixing the pictures

--- src/components/findapair.js
+++ src/components/findapair.js
@@ -1,20 +1,19 @@
 import '../css/App.css';
 import React, { useState, useEffect } from "react";
 import argentinaflag from '../images/argentinaflag_pic5.png';
-import cat from '../images/cat_pic7.png';
+import cat from '../images/cat_pic7.png.jpg';
 import coco from '../images/coco_pic6.jpg';
 import ferrari from '../images/ferrari_pic2.png';
 import formula1 from '../images/formula1_pic1.png';
 import greys from '../images/greys_pic12.png';
 import invincible from '../images/invincible_pic11.png';
-import ittakes2 from '../images/ittakestwo_pic4.png';
+import ittakes2 from '../images/ittakestwo_pic4.png.jpg';
 import leclerc from '../images/leclerc_pic3.png';
 import mate from '../images/mate_pic9.jpg';
 import messi from '../images/messi_pic10.png';
 import rubiu from '../images/rubiu_pic14.png';
-import simba from '../images/simba_pic8.png';
+import simba from '../images/simba_pic8.png.jpg';
 import spiderman from '../images/spiderman_pic15.png';
-import cardBack from '../images/card-back.png'; // Back of the card
 
 
 function FindAPair() {
